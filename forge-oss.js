3353cb37cf2b587f17014ea075d0a1a6debb0a7b
cyrillef <cyrillef@free.fr>
2019-06-02 18:58:16 +0200

    PutObject in the mix

--- forge-oss.js
+++ forge-oss.js
@@ -21,6 +21,8 @@
 module.exports = function (RED) {
     "use strict";
     var url = require('url');
+    var fs = require('fs');
+    var uuidv4 = require('uuid/v4');
     var ForgeAPI = require('forge-apis');
 
     // Forge
@@ -77,7 +79,9 @@ module.exports = function (RED) {
                 });
                 service[node.ossProperties.operation](n, node, FORGE, msg, _cb);
             } else {
-                node.error(RED._("forge.error.unknown-operation", { op: node.ossProperties.operation }));
+                node.error(RED._("forge.error.unknown-operation", {
+                    op: node.ossProperties.operation
+                }));
             }
         }
 
@@ -137,31 +141,33 @@ module.exports = function (RED) {
     service.BucketKey = function (src, out) {
         try {
             if ((src.bucketType === null && src.bucket === "") || src.bucketType === 'none') {
-                src.bucketType ="env";
-                src.bucket ="FORGE_BUCKET";
+                src.bucketType = "env";
+                src.bucket = "FORGE_BUCKET";
             }
-            out.bucket =RED.util.evaluateNodeProperty(src.bucket, src.bucketType, src, out);
+            out.bucket = RED.util.evaluateNodeProperty(src.bucket, src.bucketType, src, out);
         } catch (err) {
-            out.bucket =src.bucket;
+            out.bucket = src.bucket;
         }
     };
 
+    // GET	buckets
+    // https://forge.autodesk.com/en/docs/data/v2/reference/http/buckets-GET/
     service.ListBucketsParams = function (n, msg) {
         var params = {};
-        service.copyArg(n, "limit", params, undefined, false);
-        service.copyArg(n, "startAt", params, undefined, false);
-        service.copyArg(n, "region", params, undefined, false);
 
-        service.copyArg(msg, "limit", params, undefined, false);
-        service.copyArg(msg, "startAt", params, undefined, false);
-        service.copyArg(msg, "region", params, undefined, false);
-
-        if (params.startAt === null || params.startAt === "")
-            delete params.startAt;
-        if (params.limit === 10)
-            delete params.limit;
-        if (params.limit > 100)
-            params.limit = 100;
+        //service.getParamsSimple(n, msg, ["limit", "startAt", "region"], params);
+        service.getParams(n, msg, {
+            limit: {
+                type: "number",
+                default: [10],
+                min: 0,
+                max: 100
+            },
+            startAt: service.defaultNullOrEmptyString,
+            region: {
+                default: ["US"]
+            }
+        }, params);
         return (params);
     };
 
@@ -187,10 +193,12 @@ module.exports = function (RED) {
             });
     };
 
+    // GET	buckets/:bucketKey/details
+    // https://forge.autodesk.com/en/docs/data/v2/reference/http/buckets-:bucketKey-details-GET/
     service.BucketDetailsParams = function (n, msg) {
         var params = {};
         //service.copyArg(n, "bucket", params, undefined, false);
-        service.BucketKey (n, params);
+        service.BucketKey(n, params);
         service.copyArg(msg, "bucket", params, undefined, false);
         return (params);
     };
@@ -210,6 +218,283 @@ module.exports = function (RED) {
         //cb(null, params);
     };
 
+    // GET	buckets/:bucketKey/objects
+    // https://forge.autodesk.com/en/docs/data/v2/reference/http/buckets-:bucketKey-objects-GET/
+    service.ListObjectsParams = function (n, msg) {
+        var params = {};
+
+        //service.copyArg(n, "bucket", params, undefined, false);
+        service.BucketKey(n, params);
+        service.copyArg(msg, "bucket", params, undefined, false);
+
+        //service.getParamsSimple(n, msg, ["limit", "startAt", "beginsWidth"], params);
+        service.getParams(n, msg, {
+            limit: {
+                type: "number",
+                default: [10],
+                min: 0,
+                max: 100
+            },
+            startAt: service.defaultNullOrEmptyString,
+            beginsWith: service.defaultNullOrEmptyString
+        }, params);
+        return (params);
+    };
+
+    service.ListObjects = function (n, node, oa2legged, msg, cb) {
+        var params = service.ListObjectsParams(n, msg);
+
+        var ossObjects = new ForgeAPI.ObjectsApi();
+        ossObjects.getObjects(params.bucket, params, oa2legged, oa2legged.getCredentials())
+            .then(function (objects) {
+                //console.log(JSON.stringify(objects.body.items, null, 4));
+                if (!objects.body.hasOwnProperty('next')) {
+                    cb(null, objects);
+                } else {
+                    var url_parts = url.parse(objects.body.next, true);
+                    objects.body.nextKey = url_parts.query.startAt;
+                    objects.body.limit = params.limit || 10;
+                    objects.body.beginsWidth = params.beginsWidth || "";
+                    cb(null, objects);
+                }
+            })
+            .catch(function (error) {
+                cb(error, null);
+            });
+        //cb(null, params);
+    };
+
+    // GET	buckets/:bucketKey/objects/:objectName/details
+    // https://forge.autodesk.com/en/docs/data/v2/reference/http/buckets-:bucketKey-objects-:objectName-details-GET/
+
+    service.ObjectDetailsParams = function (n, msg) {
+        var params = {};
+        //service.copyArg(n, "bucket", params, undefined, false);
+        service.BucketKey(n, params);
+        service.copyArg(msg, "bucket", params, undefined, false);
+
+        service.getParams(n, msg, {
+            key: service.asIs,
+            with: {
+                type: "string",
+                default: [null, ""],
+                rename: "_with"
+            },
+            ifModifiedSince: service.defaultNullOrEmptyDate
+        }, params);
+
+        return (params);
+    };
+
+    service.ObjectDetails = function (n, node, oa2legged, msg, cb) {
+        var params = service.ObjectDetailsParams(n, msg);
+
+        // cyrille todo:
+        if (params._width)
+            params._width = params._with[0];
+
+        var ossObjects = new ForgeAPI.ObjectsApi();
+        ossObjects.getObjectDetails(params.bucket, params.key, params, oa2legged, oa2legged.getCredentials())
+            .then(function (obj) {
+                cb(null, obj);
+            })
+            .catch(function (error) {
+                cb(error, null);
+            });
+        //cb(null, params);
+    };
+
+    // GET	buckets/:bucketKey/objects/:objectName
+    // https://forge.autodesk.com/en/docs/data/v2/reference/http/buckets-:bucketKey-objects-:objectName-GET/
+    service.GetObjectParams = function (n, msg) {
+        var params = {};
+        //service.copyArg(n, "bucket", params, undefined, false);
+        service.BucketKey(n, params);
+        service.copyArg(msg, "bucket", params, undefined, false);
+
+        service.getParams(n, msg, {
+            key: service.asIs,
+            range: service.defaultNullOrEmptyString,
+            ifNoneMatch: service.defaultNullOrEmptyString,
+            ifModifiedSince: service.defaultNullOrEmptyDate,
+            acceptEncoding: service.defaultNullOrEmptyString
+        }, params);
+
+        return (params);
+    };
+
+    service.GetObject = function (n, node, oa2legged, msg, cb) {
+        var params = service.GetObjectParams(n, msg);
+
+        var ossObjects = new ForgeAPI.ObjectsApi();
+        ossObjects.getObject(params.bucket, params.key, params, oa2legged, oa2legged.getCredentials())
+            .then(function (obj) {
+                cb(null, obj);
+            })
+            .catch(function (error) {
+                cb(error, null);
+            });
+    };
+
+    // PutObject
+    // PUT	buckets/:bucketKey/objects/:objectName
+    // https://forge.autodesk.com/en/docs/data/v2/reference/http/buckets-:bucketKey-objects-:objectName-PUT/
+    // GET	buckets/:bucketKey/objects/:objectName/status/:sessionId
+    // https://forge.autodesk.com/en/docs/data/v2/reference/http/buckets-:bucketKey-objects-:objectName-status-:sessionId-GET/
+    // PUT	buckets/:bucketKey/objects/:objectName/resumable
+    // https://forge.autodesk.com/en/docs/data/v2/reference/http/buckets-:bucketKey-objects-:objectName-resumable-PUT/
+    service.PutObjectParams = function (n, msg) {
+        var params = {};
+        //service.copyArg(n, "bucket", params, undefined, false);
+        service.BucketKey(n, params);
+        service.copyArg(msg, "bucket", params, undefined, false);
+
+        service.getParams(n, msg, {
+            key: service.asIs,
+            localFilename: service.asIs,
+            ifMatch: service.defaultNullOrEmptyString,
+            contentType: service.defaultNullOrEmptyString,
+            contentDisposition: service.defaultNullOrEmptyString
+        }, params);
+
+        if (params.localFilename === null || params.localFilename === "" &&
+            (typeof msg.payload === 'string' || typeof msg.payload === 'object' || Buffer.isBuffer(msg.payload))
+        ) {
+            if (typeof msg.payload === "object" && !Buffer.isBuffer(msg.payload))
+                params.buffer = Buffer.from(JSON.stringify(msg.payload), 'utf8');
+            else if (typeof msg.payload === "string")
+                params.buffer = Buffer.from(msg.payload, 'utf8');
+            else
+                params.buffer = msg.payload;
+        }
+
+        return (params);
+    };
+
+    service.PutObject = function (n, node, oa2legged, msg, cb) {
+        var params = service.PutObjectParams(n, msg);
+
+        service.PutObject_put(oa2legged, params)
+            .then(function (obj) {
+                cb(null, obj);
+            })
+            .catch(function (error) {
+                cb(error, null);
+            });
+        //cb(null, params);
+    };
+
+    service.chunkSize = 5 * 1024 * 1024; // 5Mb
+    service.minChunkSize = 2 * 1024 * 1024; // 2Mb
+
+    service.PutObject_full = function (oa2legged, options, size, rstream) {
+        return (new Promise(function (fulfill, reject) {
+            var ossObjects = new ForgeAPI.ObjectsApi();
+            ossObjects.uploadObject(options.bucket, options.key, size, rstream, options, oa2legged, oa2legged.getCredentials())
+                .then(function (result) {
+                    fulfill(result);
+                })
+                .catch(function (error) {
+                    reject(error);
+                });
+        }));
+    };
+
+    service.PutObject_put = function (oa2legged, options) {
+        return (new Promise(function (fulfillMaster, rejectMaster) {
+            var ossObjects = new ForgeAPI.ObjectsApi();
+            service.filesize(options.localFilename, options.buffer)
+                .then(function (size) {
+                    if (size <= 0)
+                        throw new Error('Object size is empty!');
+                    if (size <= service.chunkSize) {
+                        var rstream = null;
+                        if ( options.localFilename !== "") {
+                            rstream =fs.createReadStream(options.localFilename);
+                        } else {
+                            rstream = new streamBuffers.ReadableStreamBuffer({
+                                frequency: 10, // in milliseconds.
+                                chunkSize: 2048 // in bytes.
+                            });
+                            rstream.put(options.buffer);
+                        }
+                        return (service.PutObject_full(oa2legged, options, size, rstream));
+                    }
+                    var nb = Math.floor(size / service.chunkSize);
+                    if ((size % service.chunkSize) !== 0)
+                        nb++;
+                    var arr = [];
+                    var uuid = uuidv4();
+                    for (var i = 0; i < nb; i++) {
+                        var start = i * service.chunkSize;
+                        var end = start + service.chunkSize - 1;
+                        if (end > size - 1)
+                            end = size - 1;
+                        var opts = {
+                            ContentRange: 'bytes ' + start + '-' + end + '/' + size,
+                            size: end - start + 1,
+                            start: start,
+                            end: end
+                        };
+
+                        // Still in parallel, but results processed in series with 'utils.promiseSerie'
+                        //arr.push (ossObjects.uploadChunk (bucketKey, ossname, service.chunkSize, opts.ContentRange, sessionId, rstream, {}, oa2legged, oa2legged.getCredentials ())) ;
+
+                        arr.push({
+                            opts: opts,
+                            options: options,
+                            bucket: options.bucket,
+                            key: options.key,
+                            sessionId: uuid,
+                            oa2legged: oa2legged
+                        });
+                    }
+
+                    return (service.promiseSerie(arr, function (item, index) {
+                        return (new Promise(function (fulfill, reject) {
+                            // If still in parallel, but results processed in series with 'utils.promiseSerie', use item.then()
+
+                            //console.log (JSON.stringify (item, null, 4)) ;
+                            var rstream = fs.createReadStream(options.localFilename, {
+                                start: item.opts.start,
+                                end: item.opts.end
+                            });
+                            ossObjects.uploadChunk(item.bucket, item.key, item.opts.size, item.opts.ContentRange, item.sessionId, rstream, item.options, item.oa2legged, item.oa2legged.getCredentials())
+                                .then(function (content) {
+                                    //console.log('Chunk ' + item.opts.ContentRange + ' accepted...');
+                                    if (content.statusCode === 202)
+                                        return (fulfill(item.opts.ContentRange));
+                                    fulfill(content);
+                                })
+                                .catch(function (error) {
+                                    reject(error);
+                                });
+                        }));
+                    }));
+
+                })
+                .then(function (info) {
+                    if (Array.isArray(info))
+                        info = info.pop();
+                    fulfillMaster(info);
+                })
+                .catch(function (error) {
+                    rejectMaster(error);
+                });
+        }));
+    };
+
+    // Utils
+    service.asIs = {};
+    service.defaultNullOrEmptyString = {
+        type: "string",
+        default: [null, ""]
+    };
+    service.defaultNullOrEmptyDate = {
+        type: "date",
+        default: [null, ""]
+    };
+
     service.copyArg = function (src, arg, out, outArg, isObject) {
         outArg = (typeof outArg !== 'undefined') ? outArg : arg; // map property
         var tmpValue = src[arg];
@@ -217,7 +502,7 @@ module.exports = function (RED) {
             if (isObject && typeof tmpValue === "string" && tmpValue !== "")
                 tmpValue = JSON.parse(stmpValue);
             out[outArg] = tmpValue;
-        } else if (src.payload.hasOwnProperty(arg) && typeof src.payload[arg] !== 'undefined') {
+        } else if (src.payload && src.payload.hasOwnProperty(arg) && typeof src.payload[arg] !== 'undefined') {
             tmpValue = src.payload[arg];
             if (isObject && typeof tmpValue === "string" && tmpValue !== "")
                 tmpValue = JSON.parse(stmpValue);
@@ -228,6 +513,86 @@ module.exports = function (RED) {
                 tmpValue = JSON.parse(stmpValue);
             out[outArg] = tmpValue;
         }
+    };
+
+    service.getParamsSimple = function (node, msg, keys, out) {
+        var params = {};
+        for (var i = 0; i < keys.length; i++) {
+            service.copyArg(node, keys[i], params, undefined, false);
+            service.copyArg(msg, keys[i], params, undefined, false);
+        }
+    };
+
+    service.getParams = function (node, msg, params, out) {
+        out = out || {};
+        var keys = Object.keys(params);
+        for (var i = 0; i < keys.length; i++) {
+            var key = keys[i];
+            service.copyArg(node, key, out, undefined, false);
+            service.copyArg(msg, key, out, undefined, false);
+
+            if (params[key].type && params[key].type !== typeof out[key]) {
+                switch (params[key].type) {
+                    case 'number':
+                        out[key] = parseFloat(out[key]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            if (params[key].default && params[key].default.includes(out[key])) {
+                delete out[key];
+                continue;
+            }
+            if (params[key].min && out[key] < params[key].min) {
+                out[key] = params[key].min;
+                continue;
+            }
+            if (params[key].max && out[key] > params[key].max) {
+                out[key] = params[key].max;
+                continue;
+            }
+
+            if (params[key].type && params[key].type === 'date') {
+                var dt = Date.parse(out[key]);
+                out[key] = new Date(dt).toUTCString();
+            }
+
+            if (params[key].rename) { // Should be last
+                out[params[key].rename] = out[key];
+                delete out[key];
+                continue;
+            }
+        }
+    };
+
+    service.filesize = function (filename, payload) {
+        return (new Promise(function (fulfill, reject) {
+            if (filename === "" && Buffer.isBuffer(payload))
+                return (fulfill(payload.length));
+
+            fs.stat(filename, function (err, stat) {
+                if (err)
+                    reject(err);
+                else
+                    fulfill(stat.size);
+            });
+        }));
+    };
+
+    // https://github.com/joliss/promise-map-series
+    service.promiseSerie = function (array, iterator, thisArg) {
+        var length = array.length;
+        var current = Promise.resolve();
+        var results = new Array(length);
+        var cb = arguments.length > 2 ? iterator.bind(thisArg) : iterator;
+        for (var i = 0; i < length; i++) {
+            current = results[i] = current.then(function (i) { // jshint ignore:line
+                return (cb(array[i], i, array));
+            }.bind(undefined, i));
+        }
+        return (Promise.all(results));
     };
 
 };
